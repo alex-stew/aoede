c2ebcd72997ffd52212ff48e2a36d13660d1ace8
Dawn Treader <69836062+alex-stew@users.noreply.github.com>
2021-06-02 14:10:56 +0930

    fix:Address middleware error making the server hang

--- utils/token.js
+++ utils/token.js
@@ -1,20 +1,23 @@
-const admin = require('../config/firebase-config')
+const admin = require("../config/firebase-config");
 
-async function decodeIDToken (req, res, next) {
-        if (req.headers?.authorization?.startsWith('Bearer ')) {
-        const idToken = req.headers.authorization.split('Bearer ')[1];
-    
-        try {
-            const decodedToken = await admin.auth().verifyIdToken(idToken);
-            req['currentUser'] = decodedToken;
-            if (decodedToken) {
-                return next();
-            }
-            return res.json({message: 'unauthorized attempt to access information'})
-        } catch (err) {
-            console.log(err);
-        }
-        }
+async function decodeIDToken(req, res, next) {
+  if (req.headers?.authorization?.startsWith("Bearer ")) {
+    const idToken = req.headers.authorization.split("Bearer ")[1];
+
+    try {
+      const decodedToken = await admin.auth().verifyIdToken(idToken);
+      req["currentUser"] = decodedToken;
+      if (decodedToken) {
+        return next();
+      }
+      return res.json({
+        message: "unauthorized attempt to access information",
+      });
+    } catch (err) {
+      console.log(err);
     }
+  }
+  next();
+}
 
-  module.exports = decodeIDToken;
+module.exports = decodeIDToken;
