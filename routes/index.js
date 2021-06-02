5a3dcd6f4c8166c54563126b6629125cf90d9e3d
Dawn Treader <69836062+alex-stew@users.noreply.github.com>
2021-06-02 13:47:57 +0930

    moved getall to routes

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,14 @@
 const router = require('express').Router();
+const path = require('path');
+require('dotenv').config();
 
 const apiRoutes = require('./api');
 
-router.use('/api', apiRoutes);
+if (process.env.NODE_ENV === "production") {
+    app.get("/*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../client/build/index.html"));
+    });
+}
+// router.use('/api', apiRoutes);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -32,9 +32,6 @@ app.use(routes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-  app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-  });
 }
 
 sequelize.sync({ force: false }).then(() => {
